87d66bccfaf4a4f01ccad48f6700fc97dabcb041
Lillian Eng <lillian.eng88@gmail.com>
2020-10-13 12:48:49 -0700

    implemented the triforce dancer class

---
+++ src/triforceDancer.js
@@ -0,0 +1,26 @@
+var TriforceDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('triforce');
+};
+
+TriforceDancer.prototype = Object.create(Dancer.prototype);
+TriforceDancer.prototype.constructor = TriforceDancer;
+
+LeftRightDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+
+  var styleSettings = {
+    height: 70,
+    width: 70
+  };
+  this.$node.css(styleSettings);
+  var self = this;
+  $('.triforce').on('mouseover', function() {
+    var styleSettings = {
+      height: 200,
+      width: 200
+    };
+    self.$node.css(styleSettings);
+  });
+
+};
